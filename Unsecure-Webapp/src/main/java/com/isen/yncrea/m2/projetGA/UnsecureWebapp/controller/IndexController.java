82d2140e612d5771f726da93c18fc81df89f2679
Valentin Magnan <19999588+valm13@users.noreply.github.com>
2021-03-20 21:46:28 +0100

    Ajout d'une BDD de 3000 credentials

--- Unsecure-Webapp/src/main/java/com/isen/yncrea/m2/projetGA/UnsecureWebapp/controller/IndexController.java
+++ Unsecure-Webapp/src/main/java/com/isen/yncrea/m2/projetGA/UnsecureWebapp/controller/IndexController.java
@@ -1,11 +1,13 @@
 package com.isen.yncrea.m2.projetGA.UnsecureWebapp.controller;
 
+import com.isen.yncrea.m2.projetGA.UnsecureWebapp.repository.AccountRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    private AccountRepository accounts;
 
     @GetMapping("/")
     public String welcome(Model model) {
