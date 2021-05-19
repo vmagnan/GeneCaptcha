3a782f77f622f815070f32fbe4b7572ddd809bab
Valentin Magnan <19999588+valm13@users.noreply.github.com>
2021-05-19 23:22:05 +0200

    Update genetic_val.py : Début de croisement (Texte --> Seulement les caractère des 2 parents sont autorisés, couleur --> Moyenne des composantes RGB)

--- pythonProject/genetic_val.py
+++ pythonProject/genetic_val.py
@@ -1,5 +1,12 @@
 from toolbox import *
 from jellyfish import levenshtein_distance
+from colorutils import *
+
+TEXT_LENGTH = 8
+WIDTH = 600
+HEIGHT = 200
+FONT_SIZE = 64
+READER = easyocr.Reader(['en'])
 
 
 class Captcha:
@@ -14,9 +21,6 @@ class Captcha:
 
 
 def initialise(number, _colors, _fonts):
-    width = 600
-    height = 200
-    font_size = 64
     captchas = []
     for i in range(number):
         bg_color = random.choice(_colors)
@@ -24,10 +28,10 @@ def initialise(number, _colors, _fonts):
         while bg_color == txt_color:
             txt_color = random.choice(_colors)
         font = random.choice(_fonts)
-        text = get_random_string(8)
+        text = get_random_string(TEXT_LENGTH)
         path = "./Image/" + "_".join([text, txt_color, bg_color, font])
-        get_new_captcha(path, text=text, color=txt_color, background=bg_color, font=font, width=width, height=height,
-                        font_size=font_size)
+        get_new_captcha(path, text=text, color=txt_color, background=bg_color, font=font, width=WIDTH, height=HEIGHT,
+                        font_size=FONT_SIZE)
         captchas.append(Captcha(text, txt_color, bg_color, font, path))
     return captchas
 
@@ -35,28 +39,63 @@ def initialise(number, _colors, _fonts):
 def evaluate(captchas):
     for captcha in captchas:
         tesseract_string = get_string_ocr_pytesseract(captcha.path)
-        easyocr_string = get_string_ocr_easyocr(captcha.path)
+        easyocr_string = get_string_ocr_easyocr(captcha.path, READER)
         captcha.value_tesseract = levenshtein_distance(tesseract_string, captcha.text)
         captcha.value_easyocr = levenshtein_distance(easyocr_string, captcha.text)
 
 
+def cross_text(text_1: string, text_2: string) -> string:
+    """
+    Cross 2 text from their used characters
+    :param text_1: Text of the 1st captcha
+    :param text_2: Text of the 2nd captcha
+    :return: Random text with allowed characters from text_1 & text_2
+    """
+    union_chars = list(set(text_1).union(set(text_2)))
+    return get_random_string(TEXT_LENGTH, union_chars)
+
+
+def cross_color(color_1_hex: string, color_2_hex: string) -> string:
+    """ Cross colors by making the average of rgb values
+    :param color_2_hex: First color in hexadecimal
+    :param color_1_hex: Second color in hexadecimal
+    :return: Average color in hexadecimal
+    """
+    r1, g1, b1 = hex_to_rgb(color_1_hex)
+    r2, g2, b2 = hex_to_rgb(color_2_hex)
+
+    rf = (r1 + r2) / 2
+    gf = (g1 + g2) / 2
+    bf = (b1 + b2) / 2
+    result_hex = rgb_to_hex((rf, gf, bf))
+    return result_hex
+
+
+def crossover(captcha_1: Captcha, captcha_2: Captcha) -> Captcha:
+    text = cross_text(captcha_1.text, captcha_2.text)
+    txt_color = cross_color(captcha_1.txt_color, captcha_2.txt_color)
+    bg_color = cross_color(captcha_1.bg_color, captcha_2.bg_color)
+    font = random.choice([captcha_1.font, captcha_2.font])
+    path = "./Image/Crossed/" + "_".join([text, txt_color, bg_color, font])
+    get_new_captcha(path, text=text, color=txt_color, background=bg_color, font=font, width=WIDTH, height=HEIGHT,
+                    font_size=FONT_SIZE)
+    print('''\
+          Crossed from : 
+          Text1 :  {text1}
+          Text2 : {text2}\
+          '''.format(text1=captcha_1.text, text2=captcha_2.text))
+
+
 # def mutate:
-def func():
-    svg_code = """
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <circle cx="12" cy="12" r="10"/>
-                <line x1="12" y1="8" x2="12" y2="12"/>
-                <line x1="12" y1="16" x2="12" y2="16"/>
-            </svg>
-        """
-    cairosvg.svg2png(bytestring=svg_code, write_to='Image/output.png')
 
 
 if __name__ == "__main__":
     number_captchas = 10
-    colors = ["black", "grey", "white", "brown", "red", "orange", "yellow", "green", "cyan", "blue", "purple", "pink"]
+    colors = ["#000000", "#808080", "#FFFFFF", "#8B4513", "#FF0000", "#ffa500", "#ffff00", "#008000", "#00ffff",
+              "#0000ff", "#800080", "#ff1493"]
     fonts = get_available_fonts()
-    # initialise(number_captchas, colors, fonts)
+    population = initialise(number_captchas, colors, fonts)
     # evaluate(captchas)
-    func()
-    print("coucou")
+    for i in range(0, number_captchas, 2):
+        crossover(population[i], population[i + 1])
+    colorutils
