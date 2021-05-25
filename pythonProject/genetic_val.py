e68892b4350680b3bc99b9019c2e9a931032edba
Valentin Magnan <19999588+valm13@users.noreply.github.com>
2021-05-25 21:53:05 +0200

    Ajout fonctions de mutation

--- pythonProject/genetic_val.py
+++ pythonProject/genetic_val.py
@@ -16,8 +16,8 @@ class Captcha:
         self.bg_color = bg_color
         self.font = font
         self.path = path
-        self.value_easyocr = 0
-        self.value_tesseract = 0
+        self.value_easyocr = -1
+        self.value_tesseract = -1
 
 
 def initialise(number, _colors, _fonts):
@@ -38,10 +38,11 @@ def initialise(number, _colors, _fonts):
 
 def evaluate(captchas):
     for captcha in captchas:
-        tesseract_string = get_string_ocr_pytesseract(captcha.path)
-        easyocr_string = get_string_ocr_easyocr(captcha.path, READER)
-        captcha.value_tesseract = levenshtein_distance(tesseract_string, captcha.text)
-        captcha.value_easyocr = levenshtein_distance(easyocr_string, captcha.text)
+        if captcha.value_easyocr != -1 or captcha.value_tesseract != -1:
+            tesseract_string = get_string_ocr_pytesseract(captcha.path)
+            easyocr_string = get_string_ocr_easyocr(captcha.path, READER)
+            captcha.value_tesseract = levenshtein_distance(tesseract_string, captcha.text)
+            captcha.value_easyocr = levenshtein_distance(easyocr_string, captcha.text)
 
 
 def cross_text(text_1: string, text_2: string) -> string:
@@ -52,26 +53,69 @@ def cross_text(text_1: string, text_2: string) -> string:
     :return: Random text with allowed characters from text_1 & text_2
     """
     union_chars = list(set(text_1).union(set(text_2)))
-    return get_random_string(TEXT_LENGTH, union_chars)
+    return mutate_text(get_random_string(TEXT_LENGTH, union_chars))
+
+
+def mutate_text(text: string) -> string:
+    """
+    Mutate a random single character of text by replacing it by one that doesn't exists in this string
+    Idea of improvement : Add the union character set of the two parents as argument, and generates a random character that doesn't appear in this union
+    :param text: Texte to mutate
+    :return: Maybe a mutated text (1/10 chance)
+    """
+    if random.randint(1, 10) == 1:
+        position = random.randint(0, len(text) - 1)
+        all_characters = string.ascii_letters + string.digits
+        allowed_characters = list(set(all_characters).difference(set(text)))
+        # Turn string to list of char because strings are immutable
+        text_characters = list(text)
+        text_characters[position] = random.choice(allowed_characters)
+        text = "".join(text_characters)
+    return text
 
 
 def cross_color(color_1_hex: string, color_2_hex: string) -> string:
     """ Cross colors by making the average of rgb values
-    :param color_2_hex: First color in hexadecimal
-    :param color_1_hex: Second color in hexadecimal
-    :return: Average color in hexadecimal
+    :param color_2_hex: First color in hexadecimal string
+    :param color_1_hex: Second color in hexadecimal string
+    :return: Average color in hexadecimal string
     """
     r1, g1, b1 = hex_to_rgb(color_1_hex)
     r2, g2, b2 = hex_to_rgb(color_2_hex)
-
     rf = (r1 + r2) / 2
     gf = (g1 + g2) / 2
     bf = (b1 + b2) / 2
     result_hex = rgb_to_hex((rf, gf, bf))
-    return result_hex
+    return mutate_color(result_hex)
+
+
+def mutate_color(color_hex: string) -> string:
+    """
+    Mutate the color
+    1/10 chance to mutate the color
+    Offset of 20 for each element
+    :param color_hex: color to mutate in hexadecimal string
+    :return: mutated color in hexadecimal string
+    """
+    if random.randint(1, 10) == 1:
+        tuple_rgb = hex_to_rgb(color_hex)
+        for v in tuple_rgb:
+            v = v + random.randint(-10, 10)
+            if v < 0:
+                v = 0
+            if v > 255:
+                v = 255
+        color_hex = rgb_to_hex(tuple_rgb)
+    return color_hex
 
 
 def crossover(captcha_1: Captcha, captcha_2: Captcha) -> Captcha:
+    """
+    Cross 2 captcha attributes to create a new one
+    :param captcha_1: First parent captcha
+    :param captcha_2: Second parent captcha
+    :return: Son captcha OR nothing if bg_color = txt_color
+    """
     text = cross_text(captcha_1.text, captcha_2.text)
     txt_color = cross_color(captcha_1.txt_color, captcha_2.txt_color)
     bg_color = cross_color(captcha_1.bg_color, captcha_2.bg_color)
@@ -86,11 +130,8 @@ def crossover(captcha_1: Captcha, captcha_2: Captcha) -> Captcha:
           '''.format(text1=captcha_1.text, text2=captcha_2.text))
 
 
-# def mutate:
-
-
 if __name__ == "__main__":
-    number_captchas = 10
+    number_captchas = 100
     colors = ["#000000", "#808080", "#FFFFFF", "#8B4513", "#FF0000", "#ffa500", "#ffff00", "#008000", "#00ffff",
               "#0000ff", "#800080", "#ff1493"]
     fonts = get_available_fonts()
@@ -98,4 +139,3 @@ if __name__ == "__main__":
     # evaluate(captchas)
     for i in range(0, number_captchas, 2):
         crossover(population[i], population[i + 1])
-    colorutils
